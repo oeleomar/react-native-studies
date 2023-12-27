08fdd3340c78276818692c6a5788ed41315632aa
oeleomar <eleomar.dev@gmail.com>
2023-12-27 16:17:09 -0400

    Image component

--- CoreTemplates/App.js
+++ CoreTemplates/App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, ImageBackground } from "react-native";
+
+const logoFile = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
@@ -9,7 +11,16 @@ export default function App() {
           <Text style={{ color: "red", fontSize: 40 }}>1...</Text>2...
         </Text>
       </View>
-      <View style={styles.view3}></View>
+      <View style={styles.view3}>
+        {/* <Image source={logoFile} style={{ width: 300, height: 300 }} />
+        <Image
+          source={{ uri: "https://picsum.photos/300" }}
+          style={{ width: 300, height: 300 }}
+        /> */}
+        {/* <ImageBackground source={logoFile} style={{ flex: 1 }}>
+        <Text>IMAGE TEXT</Text>
+      </ImageBackground> */}
+      </View>
     </View>
   );
 }
@@ -25,8 +36,6 @@ const styles = StyleSheet.create({
     backgroundColor: "lightblue",
   },
   view3: {
-    width: 200,
-    height: 200,
     backgroundColor: "lightgreen",
   },
 });

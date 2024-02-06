2115c31824286279bf319c2e95f9662048c2e7dd
Eleomar Dorneles <82125143+oeleomar@users.noreply.github.com>
2024-02-06 14:30:05 -0400

    Atualizações de páginas e remoção de tela de boas-vindas

--- app-pokemon/App.tsx
+++ app-pokemon/App.tsx
@@ -3,13 +3,15 @@ import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components";
 
 import theme from "./src/global/styles/theme";
-import { Welcome } from "./src/screens/Welcome";
+import { Welcome } from "./src/pages/Welcome";
+import { Home } from "./src/pages/Home";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar backgroundColor="transparent" translucent />
-      <Welcome />
+      {/* <Welcome /> */}
+      <Home />
     </ThemeProvider>
   );
 }

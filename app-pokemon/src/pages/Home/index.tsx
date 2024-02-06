a4406c0a79c47eabbe6a3e7dc9c2ed722d875f85
Eleomar Dorneles <82125143+oeleomar@users.noreply.github.com>
2024-02-06 16:21:33 -0400

    Adiciona dependência axios e atualiza estilos de tema

--- app-pokemon/src/pages/Home/index.tsx
+++ app-pokemon/src/pages/Home/index.tsx
@@ -1,6 +1,50 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import * as S from "./styles";
+import api from "../../service/api";
+import { Text } from "react-native";
 
 export function Home() {
-  return <S.Container></S.Container>;
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await api.get("pokemon/");
+      const { results } = response.data;
+
+      const payloadPokemon = await Promise.all(
+        results.map(async (pokemon: Pokemon) => {
+          const { id, types } = await getMoreInfo(pokemon.url);
+          return { name: pokemon.name, id, types };
+        })
+      );
+      setPokemon(payloadPokemon);
+    };
+
+    fetchData();
+  }, []);
+
+  const getMoreInfo = async (url: string) => {
+    const response = await api.get(url);
+    const { id, types } = response.data;
+    return { id, types };
+  };
+
+  return (
+    <S.Container>
+      {pokemon.map((pokemon) => (
+        <Text>{pokemon.name}</Text>
+      ))}
+    </S.Container>
+  );
 }
+
+type Pokemon = {
+  name: string;
+  url: string;
+  id: number;
+  types: PokemonTypes[];
+};
+
+type PokemonTypes = {
+  type: string;
+};
